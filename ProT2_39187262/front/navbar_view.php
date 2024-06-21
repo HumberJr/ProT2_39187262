0d2f607a19f9783affe4dec6d52f33d59a56d5bc
HumberJr <humbertojavier15@hotmail.com>
2024-06-21 16:25:07 -0300

    Add files via upload

---
+++ ProT2_39187262/front/navbar_view.php
@@ -0,0 +1,24 @@
+    <!-- INICIO BARRA DE NAVEGACION -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">BIENVENIDOS</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="principal">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="importancia_valor">Importancia y Valor</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="voluntariado">¿Queres ayudar?</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- FIN BARRA NAVEGACION -->
